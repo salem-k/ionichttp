3f0245fbd2053e022489f19acc881e3930b0538f
alibenali <ben.ali.ali.86@gmail.com>
2015-11-04 14:53:33 +0100

    couleur & image

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,20 +1,41 @@
-appContext.controller("HomeController", function(HomeService, $scope,$interval) {
+appContext.controller("HomeController", function(HomeService, $scope,$interval,$ionicLoading) {
 
 
   $interval(callAtInterval, 200);
 
-
+    /**
+     * $interval payload
+     */
      function callAtInterval() {
         HomeService.getOperation()
             .success(function(response, status, headers, config) {
                 if("color" == response[0]){
                   $scope.bgColor = "#"+response[1];
-                }else {
+                  $scope.show = false;
+                }else if("image"== response[0]) {
+                  $ionicLoading.show({
+                    template: 'Loading...'
+                  });
+                  HomeService.fileExist(response[1],function(fileName){
+                    if ("404" == fileName) {
+                      HomeService.downloadImg( response[1], "http://ec2-52-32-10-107.us-west-2.compute.amazonaws.com/back/server/"+response[1], function(imgURL){
+                        $scope.imgSrc = imgURL+"?"+new Date().getTime();
+                        $scope.show = true;
+                      });
+                    } else {
+                      console.log("------image found------- : "+fileName);
+                      $scope.imgSrc = fileName+"?"+new Date().getTime();
+                      $scope.show = true;
+                    }
 
+                  });
+                  $ionicLoading.hide();
                 }
             })
             .error(function(response, status, headers, config) {
-                console.log(response);
+                console.log("/******************/");
+                console.log(JSON.stringify(config));
+                console.log("/*******************/");
             });
-    };
+    }
 })
--- www/js/service.js
+++ www/js/service.js
@@ -1,4 +1,4 @@
-appContext.factory("HomeService", function($http) {
+appContext.factory("HomeService", function($http,$cordovaFile,$cordovaFileTransfer) {
     var getOperation = function() {
         var request = {
             url: "http://ec2-52-32-10-107.us-west-2.compute.amazonaws.com/back/server/operation.txt",
@@ -9,15 +9,54 @@ appContext.factory("HomeService", function($http) {
                 array = data.split(";");
                 return array;
             },
-            timeout: 200,
+            timeout: 1000,
         }
 
         return $http(request);
     };
 
-    var checkExistFile = function(nameFile, callBack) {
 
-      if (window.cordova) {
+    /**
+     * check if file exist
+     */
+    var fileExist = function(fileName, callBack) {
+
+        if (window.cordova) {
+            if (/Android|BlackBerry Mini/i.test(navigator.userAgent)) {
+                path = cordova.file.applicationStorageDirectory;
+            } else if (ionic.Platform.isWindowsPhone()) {
+                path = "//";
+            } else {
+                path = cordova.file.documentsDirectory;
+            }
+
+            $cordovaFile.checkFile(path, fileName)
+                .then(function(success) {
+                    // success
+                    if (window.localStorage.getItem('encours' + fileName)) {
+                        callBack("404");
+                    } else {
+                        callBack(success.nativeURL);
+                    }
+
+                }, function(error) {
+                    // error
+                    callBack("404");
+
+                });
+        }else{
+          console.warn("desktop env");
+        }
+
+    };
+
+    /**
+     * download photo serveur
+     */
+    var downloadImg = function( fileName, url, callBack) {
+
+        if (window.cordova) {
+
           if (/Android|BlackBerry Mini/i.test(navigator.userAgent)) {
               path = cordova.file.applicationStorageDirectory;
           } else if (ionic.Platform.isWindowsPhone()) {
@@ -26,26 +65,54 @@ appContext.factory("HomeService", function($http) {
               path = cordova.file.documentsDirectory;
           }
 
-          $cordovaFile.checkFile(path, nameFile)
-              .then(function(success) {
-                  // success
-                  if (window.localStorage.getItem('encours' + nameFile)) {
-                      callBack("img/photo_top_title.jpg");
-                  } else {
-                      callBack(success.nativeURL);
-                  }
+            $cordovaFile.createFile(path, fileName, true)
+                .then(function(success) {
+                    window.localStorage.setItem('encours' + fileName, true);
+                    var targetPath = success.nativeURL;
+                    var trustHosts = true;
+                    var options = {};
+                    $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+                        .then(function(result) {
+                            //successs
+                            window.localStorage.removeItem('encours' + fileName);
+                            callBack(result.nativeURL);
+                        }, function(err) {
+                            console.log('erreur download' + err.message);
+                            // Error
+                            console.log('path' + path);
+                            console.log('fileName' + fileName);
+                            $cordovaFile.removeFile(path, fileName)
+                                .then(function(success) {
+                                    window.localStorage.removeItem('encours' + fileName);
 
-              }, function(error) {
-                  // error
-                  callBack("img/photo_top_title.jpg");
+                                    callBack("404");
+                                }, function(error) {
+                                    // error
+                                    window.localStorage.removeItem('encours' + fileName);
+                                    console.log('erreur remove');
+                                    callBack("404");
+                                });
 
-              });
-            }
+                        }, function(progress) {
 
-        };
+                        });
+
+                }, function(error) {
+                    // error
+                    console.error(JSON.stringify(error));
+                    callBack("404");
+                });
+
+        } else {
+          console.log
+            callBack("404");
+        }
+    };
 
     return {
         getOperation: getOperation,
+        fileExist: fileExist,
+        downloadImg : downloadImg,
     }
 
 })
